c95b803ec68298a53cefade77ee52fba667db3ef
Tam Dang <danhtam0509@gmail.com>
2023-06-11 23:49:28 -0400

    added initial sidebar, desktop sidebar, useConversation hook, and useRoutes hook

---
+++ app/components/sidebar/Sidebar.tsx
@@ -0,0 +1,11 @@
+async function Sidebar({ children }: {
+    children: React.ReactNode;
+}) {
+    return (
+        <div className="h-full">
+            {children}
+        </div>
+    )
+}
+
+export default Sidebar;
---
+++ app/users/layout.tsx
@@ -0,0 +1,20 @@
+import DesktopSidebar from "../components/sidebar/DesktopSidebar"
+import Sidebar from "../components/sidebar/Sidebar"
+
+export default async function UsersLayout({
+    children
+}: {
+    children: React.ReactNode
+}) {
+    return (
+        
+        <Sidebar>
+            <div className="h-full">
+                <DesktopSidebar/>
+                <main className="lg:pl-20 h-full">
+                    {children}
+                </main>
+            </div>
+        </Sidebar>            
+    )
+}
