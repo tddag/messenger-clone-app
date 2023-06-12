ee09d1660d0462aca2e7a963ad2e0a9766f55b66
tddag <danhtam0509@gmail.com>
2023-06-12 18:07:56 -0400

    added actions to getSession using next-auth, passing authOptions, the getCurrentUser using prisma mongodb if session exists, finished Avatar component in the Desktop sidebar, fixed image url error by adding image domains in next.configure file

--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -1,3 +1,4 @@
+import getCurrentUser from "../actions/getCurrentUser"
 import DesktopSidebar from "../components/sidebar/DesktopSidebar"
 import MobileFooter from "../components/sidebar/MobileFooter"
 import Sidebar from "../components/sidebar/Sidebar"
@@ -7,11 +8,14 @@ export default async function UsersLayout({
 }: {
     children: React.ReactNode
 }) {
+
+    const currentUser = await getCurrentUser();
+
     return (
         
         <Sidebar>
             <div className="h-full">
-                <DesktopSidebar/>
+                <DesktopSidebar currentUser={currentUser!}/>
                 <MobileFooter/>
                 <main className="lg:pl-20 h-full">
                     {children}
