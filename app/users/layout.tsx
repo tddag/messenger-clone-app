81fc54020aa372a0426e705240bc030456f46afc
Tam Dang <danhtam0509@gmail.com>
2023-06-12 23:37:47 -0400

    added action to getUsers from mongodb using prisma, checked getSession from next-auth, finished UserList and UserBox components

--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -1,7 +1,6 @@
-import getCurrentUser from "../actions/getCurrentUser"
-import DesktopSidebar from "../components/sidebar/DesktopSidebar"
-import MobileFooter from "../components/sidebar/MobileFooter"
+import getUsers from "../actions/getUsers"
 import Sidebar from "../components/sidebar/Sidebar"
+import UserList from "./components/UserList"
 
 export default async function UsersLayout({
     children
@@ -9,17 +8,14 @@ export default async function UsersLayout({
     children: React.ReactNode
 }) {
 
-    const currentUser = await getCurrentUser();
+    const users = await getUsers()
 
     return (
         
         <Sidebar>
             <div className="h-full">
-                <DesktopSidebar currentUser={currentUser!}/>
-                <MobileFooter/>
-                <main className="lg:pl-20 h-full">
-                    {children}
-                </main>
+                <UserList items={users}/>
+                {children}
             </div>
         </Sidebar>            
     )
