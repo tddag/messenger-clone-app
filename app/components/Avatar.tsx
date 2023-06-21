94a43bd8f132822a40241b414e777d6f7b4149f8
tddag <danhtam0509@gmail.com>
2023-06-21 19:03:15 -0400

    finished real-time user active status, zustand, and pusherServer.authorizeChannel

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -4,6 +4,7 @@ import { User } from '@prisma/client';
 import Image from 'next/image';
 
 import React from 'react'
+import useActiveList from '../hooks/useActiveList';
 
 interface AvatarProps {
     user?: User;
@@ -12,6 +13,10 @@ interface AvatarProps {
 const Avatar: React.FC<AvatarProps> = ({
     user
 }) => {
+
+    const { members } = useActiveList();
+    const isActive = members.indexOf(user?.email!) !== -1;
+
   return (
     <div className="relative">
         <div className="
@@ -23,12 +28,15 @@ const Avatar: React.FC<AvatarProps> = ({
                 fill
             />
         </div>
-        <span
-            className="
-                absolute block rounded-full bg-green-500 ring-2 ring-white top-0 right-0 h-2 w-2 md:h-3 md:w-3
-            "
-        />  
+        {isActive && (
+
         
+            <span
+                className="
+                    absolute block rounded-full bg-green-500 ring-2 ring-white top-0 right-0 h-2 w-2 md:h-3 md:w-3
+                "
+            />  
+        )}
     </div>
   )
 }
